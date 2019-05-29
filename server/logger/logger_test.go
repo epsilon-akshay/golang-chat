235f6703c294946091b345fe72042aba4af6125f
akshay.nambiar <akshay.nambiar@go-jek.com>
2019-05-29 06:57:38 +0530

    Fixed logging test

--- server/logger/logger_test.go
+++ server/logger/logger_test.go
@@ -10,50 +10,50 @@ import (
 
 func TestTextTypeLogger(t *testing.T) {
 	New(os.Stdout, "text", "info")
-	assert.Equal(t, &logrus.TextFormatter{}, log.Formatter)
+	assert.Equal(t, &logrus.TextFormatter{}, Log.Formatter)
 }
 
 func TestJsonTypeLogger(t *testing.T) {
 	New(os.Stdout, "json", "info")
-	assert.Equal(t, &logrus.JSONFormatter{}, log.Formatter)
+	assert.Equal(t, &logrus.JSONFormatter{}, Log.Formatter)
 }
 
 func TestInfoLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "info")
-	assert.Equal(t, logrus.InfoLevel, log.Level)
+	assert.Equal(t, logrus.InfoLevel, Log.Level)
 }
 
 func TestWarnLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "warn")
-	assert.Equal(t, logrus.WarnLevel, log.Level)
+	assert.Equal(t, logrus.WarnLevel, Log.Level)
 }
 
 func TestDebugLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "debug")
-	assert.Equal(t, logrus.DebugLevel, log.Level)
+	assert.Equal(t, logrus.DebugLevel, Log.Level)
 }
 
 func TestErrorLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "error")
-	assert.Equal(t, logrus.ErrorLevel, log.Level)
+	assert.Equal(t, logrus.ErrorLevel, Log.Level)
 }
 
 func TestFatalLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "fatal")
-	assert.Equal(t, logrus.FatalLevel, log.Level)
+	assert.Equal(t, logrus.FatalLevel, Log.Level)
 }
 
 func TestPanicLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "panic")
-	assert.Equal(t, logrus.PanicLevel, log.Level)
+	assert.Equal(t, logrus.PanicLevel, Log.Level)
 }
 
 func TestTraceLevelLogger(t *testing.T) {
 	New(os.Stdout, "text", "trace")
-	assert.Equal(t, logrus.TraceLevel, log.Level)
+	assert.Equal(t, logrus.TraceLevel, Log.Level)
 }
 
 func TestParseError(t *testing.T) {
 	New(os.Stdout, "text", "asds")
-	assert.Equal(t, logrus.InfoLevel, log.Level)
+	assert.Equal(t, logrus.InfoLevel, Log.Level)
 }
